cd329cdf386c6043ada084e59a71a5f1cbdf516f
prof-fernando <fernando.oliveira@iffarroupilha.edu.br>
2025-05-05 16:52:46 -0300

    enviando codigos feito em aula

--- src/app/paginas/pessoa/pessoa.component.ts
+++ src/app/paginas/pessoa/pessoa.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { PessoaService, Pessoa } from '../../servicos/pessoa.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pessoa',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pessoa.component.html',
   styleUrl: './pessoa.component.scss'
 })
 export class PessoaComponent {
+
+   pessoas : Pessoa[] = [];
+
+  constructor( private servico: PessoaService ){
+    servico.listarPessoas().subscribe( (resposta) =>{
+      this.pessoas = resposta;
+    } )  ;
+  }
 
 }
